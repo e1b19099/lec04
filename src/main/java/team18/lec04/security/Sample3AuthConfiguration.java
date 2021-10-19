3530f907e857eb69ebd43a5c35d31d8d7c312e15
yasumura-kaito <e1b19100@oit.ac.jp>
2021-10-19 14:54:28 +0900

    T8の実装

--- src/main/java/team18/lec04/security/Sample3AuthConfiguration.java
+++ src/main/java/team18/lec04/security/Sample3AuthConfiguration.java
@@ -45,7 +45,6 @@ public class Sample3AuthConfiguration extends WebSecurityConfigurerAdapter {
     // antMatchers().authenticated がantMatchersへのアクセスに認証を行うことを示す
     // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
     // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
-    http.authorizeRequests().antMatchers("/sample3/**").authenticated();
     http.authorizeRequests().antMatchers("/sample4/**").authenticated();
 
     // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
