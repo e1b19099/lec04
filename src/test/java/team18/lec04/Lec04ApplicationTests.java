8d231819fd71cf494ed09bb36f0e505363921043
e1b19099 <e1b19099@oit.ac.jp>
2021-10-19 14:14:28 +0900

    リポジトリの初期化

---
+++ src/test/java/team18/lec04/Lec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package team18.lec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
